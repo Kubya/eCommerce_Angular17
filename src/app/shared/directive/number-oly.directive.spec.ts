37e177f4ac7ce4cb17e8487ece757008fa101166
kubya <sabavath.kubya93@gmail.com>
2025-11-19 11:02:01 +0530

    Reset and keep local project

--- src/app/shared/directive/number-oly.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumberOlyDirective } from './number-oly.directive';
-
-describe('NumberOlyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NumberOlyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
